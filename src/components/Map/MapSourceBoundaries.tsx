09c7ed7effb0fde12aa319d57e0cb21dab4d16cd
Tobias <tobias@fixmycity.de>
2023-09-14 10:54:51 +0200

    Revert "Work around reactmapgl import issue with Bun.sh"
    This reverts commit a6eb45ef42cdeccab8aad0006c1c560fc8c03f00.

--- src/components/Map/MapSourceBoundaries.tsx
+++ src/components/Map/MapSourceBoundaries.tsx
@@ -1,4 +1,4 @@
-import * as reactmapgl from 'react-map-gl/dist/es5/exports-maplibre.js'
+import * as reactmapgl from 'react-map-gl/maplibre'
 
 export const MapSourceBoundaries = () => {
   return (
